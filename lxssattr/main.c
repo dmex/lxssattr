ee4a020c7e9f53002b6f70ef308aa2c272bf55d1
dmex <dmex04@gmail.com>
2017-04-13 04:29:38 +1000

    Fix querying directory attributes, Fix crash

--- lxssattr/main.c
+++ lxssattr/main.c
@@ -22,18 +22,17 @@ void __cdecl _tmain()
     NTSTATUS status;
     IO_STATUS_BLOCK isb;
     OBJECT_ATTRIBUTES oa;
-    UNICODE_STRING fileName;
+    UNICODE_STRING fileName = { 0 };
     HANDLE fileHandle = NULL;
     FILE_EA_INFORMATION fileEaInfo = { 0 };
     PFILE_FULL_EA_INFORMATION buffer = NULL;
     ULONG bufferLength = 0;
 
-    SetConsoleTitle(_T("lxssattr v1.1 by dmex - LXSS extended file attributes viewer"));
+    SetConsoleTitle(_T("lxssattr v1.2 by dmex - LXSS extended file attributes viewer"));
 
     if (__argc != 2)
     {
         _tprintf(_T("Invalid arguments.\n"));
-        //_gettch();
         return;
     }
 
@@ -42,148 +41,137 @@ void __cdecl _tmain()
     LxssLoadUsersFile();
     LxssLoadGroupsFile();
 
-    __try
+    if (!NT_SUCCESS(status = RtlDosPathNameToNtPathName_U_WithStatus(
+        __targv[1],
+        &fileName,
+        NULL,
+        NULL
+        )))
     {
-        if (!NT_SUCCESS(status = RtlDosPathNameToNtPathName_U_WithStatus(
-            __targv[1],
-            &fileName,
-            NULL,
-            NULL
-            )))
-        {
-            _tprintf(_T("[ERROR] RtlDosPathNameToNtPathName: 0x%x\n"), status);
-            __leave;
-        }
+        _tprintf(_T("[ERROR] RtlDosPathNameToNtPathName: 0x%x\n"), status);
+        goto CleanupExit;
+    }
 
-        InitializeObjectAttributes(
-            &oa,
-            &fileName,
-            OBJ_CASE_INSENSITIVE,
-            NULL,
-            NULL
+    InitializeObjectAttributes(
+        &oa,
+        &fileName,
+        OBJ_CASE_INSENSITIVE,
+        NULL,
+        NULL
+        );
+
+    if (!NT_SUCCESS(status = NtOpenFile(
+        &fileHandle,
+        FILE_GENERIC_READ, // includes the required FILE_READ_EA access_mask!
+        &oa,
+        &isb,
+        FILE_SHARE_READ,
+        FILE_SYNCHRONOUS_IO_NONALERT
+        )))
+    {
+        _tprintf(_T("[ERROR] NtOpenFile: 0x%x\n"), status);
+        goto CleanupExit;
+    }
+
+    //if ((fileHandle = CreateFile(
+    //    __targv[1],
+    //    FILE_GENERIC_READ, // includes the required FILE_READ_EA access_mask!
+    //    FILE_SHARE_READ,
+    //    NULL,
+    //    OPEN_EXISTING,
+    //    0,
+    //    NULL
+    //    )) == INVALID_HANDLE_VALUE)
+    //{
+    //    _tprintf(_T("[ERROR] CreateFile: 0x%x\n"), GetLastError());
+    //    __leave;
+    //}
+
+    // Query the Extended Attribute length
+    if (!NT_SUCCESS(status = NtQueryInformationFile(
+        fileHandle,
+        &isb,
+        &fileEaInfo,
+        sizeof(FILE_EA_INFORMATION),
+        FileEaInformation
+        )))
+    {
+        _tprintf(_T("[ERROR] NtQueryInformationFile: 0x%x\n"), status);
+        goto CleanupExit;
+    }
+
+    // Allocate memory for the Extended Attribute
+    bufferLength = fileEaInfo.EaSize;
+    buffer = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, bufferLength + 1);
+
+    // Query the Extended Attribute structure.
+    if (!NT_SUCCESS(status = NtQueryEaFile(
+        fileHandle,
+        &isb,
+        buffer,
+        bufferLength,
+        TRUE, // return only the first entry that is found
+        NULL,
+        0,
+        NULL,
+        FALSE
+        )))
+    {
+        //if (status == STATUS_NO_MORE_EAS)
+        //if (status == STATUS_NO_EAS_ON_FILE)
+        _tprintf(_T("[ERROR] NtQueryEaFile: 0x%x\n"), status);
+        goto CleanupExit;
+    }
+
+    if (_stricmp("LXATTRB", buffer->EaName))
+    {
+        _tprintf(_T("[ERROR] LXATTRB not found.\n"));
+        goto CleanupExit;
+    }
+    else
+    {
+        LXSS_FILE_EXTENDED_ATTRIBUTES_V1 extendedAttr;
+
+        // Make temporary copy of the structure
+        RtlZeroMemory(&extendedAttr, sizeof(LXSS_FILE_EXTENDED_ATTRIBUTES_V1));
+        RtlCopyMemory(
+            &extendedAttr,
+            buffer->EaName + (buffer->EaNameLength + 1),
+            sizeof(LXSS_FILE_EXTENDED_ATTRIBUTES_V1)
             );
 
-        if (!NT_SUCCESS(status = NtOpenFile(
-            &fileHandle, 
-            FILE_GENERIC_READ, // includes the required FILE_READ_EA access_mask!
-            &oa,
-            &isb,
-            FILE_SHARE_READ,
-            FILE_SYNCHRONOUS_IO_NONALERT | FILE_NON_DIRECTORY_FILE
-            )))
-        {
-            _tprintf(_T("[ERROR] NtOpenFile: 0x%x\n"), status);
-            __leave;
-        }
+        //_tprintf(_T("LXSS Attributes:\n"));
+        _tprintf(_T("Flags:                     %hu\n"), extendedAttr.Flags);
+        _tprintf(_T("Version:                   %hu\n"), extendedAttr.Version);
+        _tprintf(_T("Mode:                      %o (octal)\n"), extendedAttr.st_mode);
+        _tprintf(_T("Ownership:                 Uid: (%lu / %s), Gid: (%lu / %s)\n"),
+            extendedAttr.st_uid,
+            GetUserNameFromUid(extendedAttr.st_uid),
+            extendedAttr.st_gid,
+            GetGroupNameFromGid(extendedAttr.st_gid)
+            );
+        _tprintf(_T("Access:                    (0%o) %hs\n"),
+            extendedAttr.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO),
+            lsperms(extendedAttr.st_mode)
+            );
+        _tprintf(_T("Last status change:        %s\n"), UnixStatTime(extendedAttr.st_ctime, extendedAttr.st_ctime_nsec));
+        _tprintf(_T("Last file access:          %s\n"), UnixStatTime(extendedAttr.st_atime, extendedAttr.st_atime_nsec));
+        _tprintf(_T("Last file modification:    %s\n"), UnixStatTime(extendedAttr.st_mtime, extendedAttr.st_mtime_nsec));
 
-        // Note: If you don't want to use NtOpenFile, InitializeObjectAttributes or RtlDosPathNameToNtPathName_U_WithStatus...
-        // Just remove the above code and use CreateFile.
-        //if ((fileHandle = CreateFile(
-        //    __targv[1],
-        //    FILE_GENERIC_READ, // includes the required FILE_READ_EA access_mask!
-        //    FILE_SHARE_READ,
-        //    NULL,
-        //    OPEN_EXISTING,
-        //    0,
-        //    NULL
-        //    )) == INVALID_HANDLE_VALUE)
-        //{
-        //    _tprintf(_T("[ERROR] CreateFile: 0x%x\n"), GetLastError());
-        //    __leave;
-        //}
-
-        // Query the Extended Attribute length
-        if (!NT_SUCCESS(status = NtQueryInformationFile(
-            fileHandle,
-            &isb,
-            &fileEaInfo,
-            sizeof(FILE_EA_INFORMATION),
-            FileEaInformation
-            )))
-        {
-            _tprintf(_T("[ERROR] NtQueryInformationFile: 0x%x\n"), status);
-            __leave;
-        }
-
-        // Allocate memory for the Extended Attribute
-        bufferLength = fileEaInfo.EaSize;
-        buffer = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, bufferLength + 1);
-
-        // Query the Extended Attribute structure.
-        if (!NT_SUCCESS(status = NtQueryEaFile(
-            fileHandle, 
-            &isb,
-            buffer, 
-            bufferLength,
-            TRUE, // return only the first entry that is found
-            NULL, 
-            0, 
-            NULL, 
-            FALSE
-            )))
-        {
-            //if (status == STATUS_NO_MORE_EAS)
-            //if (status == STATUS_NO_EAS_ON_FILE)
-            _tprintf(_T("[ERROR] NtQueryEaFile: 0x%x\n"), status);
-            __leave;
-        }
-
-        if (_stricmp("LXATTRB", buffer->EaName))
-        {
-            _tprintf(_T("[ERROR] LXATTRB not found.\n"));
-            __leave;
-        }
-        else
-        {
-            LXSS_FILE_EXTENDED_ATTRIBUTES_V1 extendedAttr;
-
-            // Make temporary copy of the structure
-            RtlZeroMemory(&extendedAttr, sizeof(LXSS_FILE_EXTENDED_ATTRIBUTES_V1));
-            RtlCopyMemory(
-                &extendedAttr,
-                buffer->EaName + (buffer->EaNameLength + 1),
-                sizeof(LXSS_FILE_EXTENDED_ATTRIBUTES_V1)
-                );
-
-            //_tprintf(_T("LXSS Attributes:\n"));
-            _tprintf(_T("Flags:                     %hu\n"), extendedAttr.Flags);
-            _tprintf(_T("Version:                   %hu\n"), extendedAttr.Version);
-            _tprintf(_T("Mode:                      %o (octal)\n"), extendedAttr.st_mode);
-            _tprintf(_T("Ownership:                 Uid: (%lu / %s), Gid: (%lu / %s)\n"),
-                extendedAttr.st_uid, 
-                GetUserNameFromUid(extendedAttr.st_uid), 
-                extendedAttr.st_gid, 
-                GetGroupNameFromGid(extendedAttr.st_gid)
-                );
-            _tprintf(_T("Access:                    (0%o) %hs\n"),
-                extendedAttr.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO), 
-                lsperms(extendedAttr.st_mode)
-                );        
-            _tprintf(_T("Last status change:        %s\n"), UnixStatTime(extendedAttr.st_ctime, extendedAttr.st_ctime_nsec));
-            _tprintf(_T("Last file access:          %s\n"), UnixStatTime(extendedAttr.st_atime, extendedAttr.st_atime_nsec));
-            _tprintf(_T("Last file modification:    %s\n"), UnixStatTime(extendedAttr.st_mtime, extendedAttr.st_mtime_nsec));
-
-            // Debug helper
-            //DumpEaInformaton(buffer);
-        }
+        // Debug helper
+        //DumpEaInformaton(buffer);
     }
-    __finally
-    {
-        if (fileHandle)
-        {
-            NtClose(fileHandle);
-        }
 
-        if (fileName.Buffer)
-        {
-            RtlFreeHeap(GetProcessHeap(), 0, fileName.Buffer);
-        }
+CleanupExit:
 
-        if (buffer)
-        {
-            HeapFree(GetProcessHeap(), 0, buffer);
-        }
-    }
+    if (fileHandle)
+        NtClose(fileHandle);
+
+    if (fileName.Buffer)
+        RtlFreeHeap(GetProcessHeap(), 0, fileName.Buffer);
+
+    if (buffer)
+        HeapFree(GetProcessHeap(), 0, buffer);
 
     //_tprintf(_T("Press any key to continue...\n"));
     //_gettch();
